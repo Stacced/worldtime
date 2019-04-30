6a96f57c6d9fc3efb8924ea53adfda1afe77eb94
Batman but less secret <20029433+stacced@users.noreply.github.com>
2019-04-30 08:47:50 +0200

    core: base files

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <title>WorldTime</title>
+</head>
+<body>
+</body>
+</html>
