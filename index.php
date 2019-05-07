61c04eabda73bfba710456fd3c41e5e01f9035aa
Adrian Spy <49429806+andria01@users.noreply.github.com>
2019-05-07 08:34:27 +0200

    core: added css

--- index.php
+++ index.php
@@ -4,10 +4,22 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <link rel="stylesheet" href="css/style.css" type="text/css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <title>WorldTime</title>
 </head>
 <body>
-    <button class="btnGps">Enable Geolocation</button>
+	<header>
+		<h1> WORLD CLOCK </h1>
+	</header>
+	<div id="containers">
+		<form action="#" method="POST">
+			<div id="Containers_Form">
+				<input id="SearchBar_I" type="search">
+				<i class="fa fa-search"></i>
+			</div>
+			<img id="location_GPS" class="btnGps" src="img/Location.png" alt="location GPS"/>
+		</form>
+	</div>
 </body>
 <script src="js/geoMap.js"></script>
 </html>
