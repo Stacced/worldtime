5d6765bd73b49ba5487c6920cd60ff43e40367c2
Adrian Spy <49429806+andria01@users.noreply.github.com>
2019-05-14 10:51:30 +0200

    core: modify horloge digital

--- js/clockHandler.js
+++ js/clockHandler.js
@@ -117,27 +117,56 @@ function moveSecondHands() {
 }
 
 function showTime(){
-    let h = date.getHours(); // 0 - 23
-    let m = date.getMinutes(); // 0 - 59
-    let s = date.getSeconds(); // 0 - 59
-    let session = "AM";
-
-    if (h === 0){
-        h = 12;
-    }
-
-    if (h > 12){
-        h = h - 12;
-        session = "PM";
-    }
-
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
-
-    let time = h + ":" + m + ":" + s + " " + session;
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    
+    var tempH = "";
+    var tempM = "";
+    var tempS = "";
+        
+    tempH = (h < 10) ? "0" + h : h;
+    tempM = (m < 10) ? "0" + m : m;
+    tempS = (s < 10) ? "0" + s : s;
+    
+    var time = tempH + ":" + tempM + ":" + tempS;
     document.getElementById("clock-digital").innerText = time;
     document.getElementById("clock-digital").textContent = time;
+    
+    RefreshTime(h, m, s);
+}
 
-    setTimeout(() => {showTime()}, 1000);
+function RefreshTime(h, m, s) {
+    console.log("Appel Refresh");
+    
+    var tempH = "";
+    var tempM = "";
+    var tempS = "";
+    
+    s += 1;
+    
+    if (s > 59) {
+        s = 0;
+        m += 1;
+        
+        if (m > 59) {
+            m = 0;
+            h += 1;
+            
+            if (h > 23) {
+                h = 0;
+            }
+        }
+    }
+            
+    tempH = (h < 10) ? "0" + h : h;
+    tempM = (m < 10) ? "0" + m : m;
+    tempS = (s < 10) ? "0" + s : s;
+    
+    var time = tempH + ":" + tempM + ":" + tempS;
+    document.getElementById("clock-digital").innerText = time;
+    document.getElementById("clock-digital").textContent = time;
+    
+    setTimeout(() => {RefreshTime(h, m, s)}, 1000);
 }
